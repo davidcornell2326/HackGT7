c456af7512db4f96b031f1b8cbc8d9b582ee516a
David Cornell <davidcornell2326@gmail.com>
2020-10-17 16:47:33 -0400

    Made document and input routing work, supports Constitution and Declaration of Independence

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -55,26 +55,13 @@ public class Main {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
-
-
+  
 
   @RequestMapping("/")
   String index() {
     return "home";
   }
 
-  @RequestMapping("/testpage")
-  String testpage() {
-    return "testpage";
-  }
-
-  @RequestMapping("/hello")
-  String hello(Map<String, Object> model) {
-      RelativisticModel.select();
-      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-      model.put("science", "E=mc^2: 12 GeV = " + m.toString());
-      return "hello";
-  }
 
   @RequestMapping("/userEntry")
   String userEntry() {
@@ -83,34 +70,14 @@ public class Main {
   
   @RequestMapping("/documentDisplay/*")
   String documentDisplay() {
-      return "documentDisplay";
+      return "result";
   }
 
-  @RequestMapping("/Documents")
-  String Documents() {
-      return "Documents";
+  @RequestMapping("/documents")
+  String documents() {
+      return "documents";
   }
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
 
   @Bean
   public DataSource dataSource() throws SQLException {
